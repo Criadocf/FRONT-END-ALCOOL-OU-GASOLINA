a82be88e41e03f55d97b6b927ec12fbeadada537
kleber junior <kleberjrmmxvi@gmail.com>
2022-09-26 21:14:15 -0300

    CONTINUACAO ALCOOL OU GASOLINA

--- comportamento.js
+++ comportamento.js
@@ -1,2 +1,19 @@
-var n1 = "Olá Mundo!"
-window.alert(n1)
+const valor_alcool = Number(document.getElementById('alcool').value)
+const valor_gasolina = Number(document.getElementById('gasolina'). value)
+const perfomance = Number(document.getElementById('perfomance'))
+const dinheiro_disponivel = Number(document.getElementById('dinheiro').value)
+const local_destino = (document.getElementById('local').value)
+const km_a_percorrer = Number(document.getElementById('km'))
+const analise = Number(document.getElementById('veranalise').value)
+
+const perfomance_alcool = km_lit_gas*0.7
+
+function calcular(){
+    const litros_gasolina = km_a_percorrer/perfomance
+    const dinheiro_gasolina = valor_gasolina/km_a_percorrer
+    const perfomance_gasolina = perfomance
+    const 
+}
+
+
+con

ba67d4458dc6443122a52015e202229ce4073e98
kleber <kleberjrmmxvi@gmail.com>
2022-10-02 16:55:30 -0300

    ATUALIZACAO

--- comportamento.js
+++ comportamento.js
@@ -1,19 +1,27 @@
-const valor_alcool = Number(document.getElementById('alcool').value)
-const valor_gasolina = Number(document.getElementById('gasolina'). value)
-const perfomance = Number(document.getElementById('perfomance'))
-const dinheiro_disponivel = Number(document.getElementById('dinheiro').value)
-const local_destino = (document.getElementById('local').value)
-const km_a_percorrer = Number(document.getElementById('km'))
-const analise = Number(document.getElementById('veranalise').value)
+const valor_gasolina = document.getElementById('gasolina')
+const valor_alcool = document.getElementById('alcool')
+const perfomance = document.getElementById('perfomance')
+const dinheiro_disponivel = document.getElementById('dinheiro')
+const local_destino = document.getElementById('local')
+const km_a_percorrer = document.getElementById('km')
 
-const perfomance_alcool = km_lit_gas*0.7
+const enviar = document.getElementById('calcular')
 
-function calcular(){
-    const litros_gasolina = km_a_percorrer/perfomance
-    const dinheiro_gasolina = valor_gasolina/km_a_percorrer
-    const perfomance_gasolina = perfomance
-    const 
-}
+enviar.addEventListener('click', ()=>{
+    const gasolina = Number(valor_gasolina.value)
+    const alcool = Number(valor_alcool.value)
+    const performanc_gasolina = Number(performance.value)
+    const dinheiro = Number(dinheiro_disponivel.value)
+    const destino = (local_destino.innerText)
+    const distancia = Number(km_a_percorrer.value)
 
+    /*GASOLINA*/
 
-con
+    const litros_gas_neces = Number(performanc_gasolina/distancia)
+    const dinheiro_gas_necess = Number(gasolina*litros_gas_neces)
+    const valor_a_completar_gas = Number(dinheiro_gas_necess-dinheiro)
+    const troco_gas = Number(dinheiro-dinheiro_gas_necess)
+
+    dinheiro<dinheiro_gas_necess?alert(`Mano, você tá quebrado, dá um jeito de arrumar o restante da grana, mais precisamente falando tu tem que arrumar mais R$${valor_a_completar_gas}`):alert(`Mano, tua grana é suficiente, e ainda vai te sobrar R$${troco_gas}`)
+
+    })
